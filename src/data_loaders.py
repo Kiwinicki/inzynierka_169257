623f363773e95ef280ecd0680c1677ba824bf1af
Kiwinicki <szefek24@gmail.com>
2026-01-05 13:16:51 +0100

    add more augmentations

--- src/data_loaders.py
+++ src/data_loaders.py
@@ -12,7 +12,13 @@ def get_data_loaders(
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(15),
             transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+            transforms.RandomResizedCrop(48, scale=(0.8, 1.0)),
+            transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), shear=15),
+            transforms.RandomPerspective(distortion_scale=0.2, p=0.5),
+            transforms.RandomAutocontrast(p=0.5),
+            transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.5),
             transforms.ToTensor(),
+            transforms.RandomErasing(p=0.5, scale=(0.02, 0.1)),
             transforms.Normalize(mean=[0.5], std=[0.5]),
         ]
     )
